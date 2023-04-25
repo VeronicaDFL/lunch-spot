27e72ac947c71f00cc0a012485b08eee6f3cae4e
Veronica De Felice <verodefelic@gmail.com>
2023-04-25 10:47:23 -0400

    FIRST commit

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import RestaurantList from './components/RestaurantList.jsx';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <RestaurantList/>
     </View>
   );
 }
@@ -13,8 +12,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'skyblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+    
